9a2589c1662e6d1a91043387fd7c9dd99b82b5a8
shiva.sharma <shs101@ucsd.edu>
2018-11-08 14:24:40 -0800

    Ability to see preference in header after selection

---
+++ js/preferences.js
@@ -0,0 +1,26 @@
+$('#dietForm input').on('change', function() {
+   var target = $('#dietButton');
+   target.html("Diet: " + $('input[name=radio1]:checked', '#dietForm').val()); 
+   console.log("Diet: " + $('input[name=radio1]:checked', '#dietForm').val());
+});
+
+
+$('#carbForm input').on('change', function() {
+   var target = $('#carbButton');
+   target.html("Carbohydrates: " + $('input[name=radio2]:checked', '#carbForm').val()); 
+   console.log("Carbohydrates: " + $('input[name=radio2]:checked', '#carbForm').val());
+});
+
+$('#proteinForm input').on('change', function() {
+   var target = $('#proteinButton');
+   target.html("Protein: " + $('input[name=radio3]:checked', '#proteinForm').val()); 
+   console.log("Protein: " + $('input[name=radio3]:checked', '#proteinForm').val());
+});
+
+/*
+$('#form input').on('change', function() {
+   var target = $('#dietButton');
+   target.html("Diet: " + $('input[name=exampleRadios]:checked', '#form').val()); 
+   console.log("Diet: " + $('input[name=exampleRadios]:checked', '#form').val());
+});
+*/
