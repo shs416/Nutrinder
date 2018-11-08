cb384d0abef7d545b5e9849377c91b4aed58911b
Vivalize <markomarkor@gmail.com>
2018-11-08 15:55:57 -0800

    Preferences now get saved into cookies

--- js/preferences.js
+++ js/preferences.js
@@ -1,20 +1,44 @@
-$('#dietForm input').on('change', function() {
-   var target = $('#dietButton');
-   target.html("Diet: " + $('input[name=radio1]:checked', '#dietForm').val()); 
-   /*console.log("Diet: " + $('input[name=radio1]:checked', '#dietForm').val());*/
+var preferences = {};
+if (Cookies.get('preferences')) {
+    preferences = JSON.parse(Cookies.get('preferences'));
+    
+    $(":radio[value=\'"+preferences.diet+"\']", "#dietForm").prop("checked", true);
+    $('#dietButton').html("Diet: " + preferences.diet);
+                          
+    $(":radio[value=\'"+preferences.carbs+"\']", "#carbForm").prop("checked", true);
+    $('#carbButton').html("Carbohydrates: " + preferences.carbs);
+                          
+    $(":radio[value=\'"+preferences.protein+"\']", "#proteinForm").prop("checked", true);
+    $('#proteinButton').html("Protein: " + preferences.protein);
+}
+
+function savePreferences() {
+    Cookies.set('preferences', JSON.stringify(preferences));
+}
+
+$('#dietForm input').on('change', function () {
+    var target = $('#dietButton');
+    target.html("Diet: " + $('input[name=radio1]:checked', '#dietForm').val());
+    preferences.diet = $('input[name=radio1]:checked', '#dietForm').val();
+    savePreferences();
+    //    console.log($('input[name=radio1]:checked', '#dietForm').val());
 });
 
 
-$('#carbForm input').on('change', function() {
-   var target = $('#carbButton');
-   target.html("Carbohydrates: " + $('input[name=radio2]:checked', '#carbForm').val()); 
-   /*console.log("Carbohydrates: " + $('input[name=radio2]:checked', '#carbForm').val());*/
+$('#carbForm input').on('change', function () {
+    var target = $('#carbButton');
+    target.html("Carbohydrates: " + $('input[name=radio2]:checked', '#carbForm').val());
+    preferences.carbs = $('input[name=radio2]:checked', '#carbForm').val();
+    savePreferences();
+    /*console.log("Carbohydrates: " + $('input[name=radio2]:checked', '#carbForm').val());*/
 });
 
-$('#proteinForm input').on('change', function() {
-   var target = $('#proteinButton');
-   target.html("Protein: " + $('input[name=radio3]:checked', '#proteinForm').val()); 
-   /*console.log("Protein: " + $('input[name=radio3]:checked', '#proteinForm').val());*/
+$('#proteinForm input').on('change', function () {
+    var target = $('#proteinButton');
+    target.html("Protein: " + $('input[name=radio3]:checked', '#proteinForm').val());
+    preferences.protein = $('input[name=radio3]:checked', '#proteinForm').val();
+    savePreferences();
+    /*console.log("Protein: " + $('input[name=radio3]:checked', '#proteinForm').val());*/
 });
 
 /*
